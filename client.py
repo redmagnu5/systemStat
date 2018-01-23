2669ad10167dc0bdc7020b30dc34ba0e936194e7
Ethan <35015514+redmagnu5@users.noreply.github.com>
2018-01-23 00:24:26 -0600

    Update client.py
    Reorganized code to make easier to understand, wrapping code into functions where appropriate.

--- client.py
+++ client.py
@@ -1,39 +1,39 @@
-# Create socket and connect
-import socket             
-s = socket.socket()         
-s.connect(('127.0.0.1', 22))
- 
-#receive data from the server
-print s.recv(1024)
+# -*- coding: utf-8 -*-
+"""
 
-# get system information
-import win32api
-userName = win32api.GetUserName()
-sysInfo = win32api.GetSystemInfo()
+@author: redMagnu5
+"""
 
-#Retrieve memory usage %
-memStat = win32api.GlobalMemoryStatus()
-memUsage = memStat['MemoryLoad']
 
-#retrieve system-wide CPU utilization as %
-import psutil
-cpuUsage = psutil.cpu_percent(interval=1)
+# Create client socket and connect to server
+def Socket():
+    import socket
+    from server import Config             
+    s = socket.socket()         
+    s.connect((Config.IP, Config.port))
+    print s.recv(10000)
+    s.send(sysStat())
+    print s.recv(10000)
+    return s
 
-#retrieve system up time (YYYY-MM-DD hr:min:secs)
-import datetime
-sysUptime = datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")
+#Gathers client system statistics 
+def sysStat():    
+    import win32api
+    import psutil
+    import socket
+    import datetime
+    import json
+    userName = win32api.GetUserName()
+    memStat = win32api.GlobalMemoryStatus()
+    memUsage = memStat['MemoryLoad']
+    cpuUsage = psutil.cpu_percent(interval=1)
+    clientIp = socket.gethostbyname(socket.gethostname())
+    sysUptime = datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")
+    stat = [userName, clientIp, memUsage, cpuUsage, sysUptime]
+    sysStat = json.dumps(stat)
+    return sysStat
 
-#retrieve client ip
-clientIp = socket.gethostbyname(socket.gethostname())
-
-#Compile all system stats in a list, serialize it to send over socket
-import json
-stat = [memUsage, cpuUsage, sysUptime, userName, clientIp]
-sysStat = json.dumps(stat)
-
-print sysStat
-
-#send all stats over socket
-s.send(sysStat)
-print s.recv(1024)
+    
+sysStat()
+Socket()
 
